4ea59723a1b2cd5c4f31fc3868f1bb74b566d103
Dc Dev <96603256+DanielDcDev@users.noreply.github.com>
2023-10-12 15:55:56 +0100

    update day 0

---
+++ versaoAtual/Scripts/backEnd/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const server = express();
+
+server.get('/usuario', (req, res) =>{
+    return res.json({usuario: 'Daniel Caio'})
+})
+
+server.listen(3000, ()=>{
+    console.log("servidor esta funcionando...")
+})
