ea3841a958e3c0e0c6573febdbc38106a32287ee
Dc Dev <96603256+DanielDcDev@users.noreply.github.com>
2023-10-12 15:56:50 +0100

    .

--- versaoAtual/Scripts/backEnd/index.js
+++ versaoAtual/Scripts/backEnd/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const server = express();
-
+const shoes = require('./')
 server.get('/usuario', (req, res) =>{
     return res.json({usuario: 'Daniel Caio'})
 })
